04163d5bf4b9b617e3f577100b30e5b8b2b21292
Derek Pauley <droc37191@gmail.com>
2015-05-22 10:23:38 -0700

    Cleaning up

--- src/main/java/spring1/MvcConfig.java
+++ src/main/java/spring1/MvcConfig.java
@@ -1,10 +1,8 @@
 package spring1;
 
-import java.util.List;
+
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -18,9 +16,4 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		registry.addViewController("/dashboard").setViewName("dashboard");
 	}
 	
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		GsonHttpMessageConverter GsonConverter = new GsonHttpMessageConverter();
-		converters.add(GsonConverter);
-	}
 }
--- src/test/java/spring1/Spring1ApplicationTests.java
+++ src/test/java/spring1/Spring1ApplicationTests.java
@@ -1,68 +1,68 @@
 package spring1;
 
-//All gonna fail. I'm using a @Component service now
 
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNotNull;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-////import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = Spring1Application.class)
-//@Transactional
-//@WebAppConfiguration
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Spring1Application.class)
+@Transactional
+@WebAppConfiguration
 public class Spring1ApplicationTests {
 
-//	@Autowired
-//	private ContactRepository contactRepo;
-//	
-//	Contact contact1;
-//	
-//	@Before
-//	public void setUp() {
-//		contact1 = new Contact("Derek", "333-333-3333", "123 my address");
-//		contactRepo.deleteAll();
-//		contactRepo.save(contact1);
-//		
-//	}
-//	
-//	@Test
-//	public void getContactByNameRepoTest() {
-//		
-//		final String NAMETOTEST = "Derek";
-//		Contact contactToTestByName = contactRepo.findByName(NAMETOTEST);
-//		assertNotNull(contactToTestByName);
-//		assertEquals(contactToTestByName.getName(), NAMETOTEST);
-//		
-//	} 
-//	
-//	@Test
-//	public void getContactByAddressRepoTest() {
-//		final String ADDRESSTOTEST = "123 my address";
-//		Contact contactToTestByAddress = contactRepo.findByAddress(ADDRESSTOTEST);
-//		assertNotNull(contactToTestByAddress);
-//		assertEquals(contactToTestByAddress.getAddress(), ADDRESSTOTEST);
-//	}
-//	
-//	@Test
-//	public void createContactTest() {
-//		final String nameToExpect = "Libby";
-//		Contact contactToTest = new Contact("Libby", "444-444-4444", "456 my address");
-//		contactRepo.save(contactToTest);
-//		Contact confirmContactToTest = contactRepo.findByName(nameToExpect);
-//		assertNotNull(confirmContactToTest);
-//		assertEquals(nameToExpect, confirmContactToTest.getName(), contactToTest.getName());
-//	}
-//	
-	//TODO -- Test Controller Methods 5/5/2015
+	@Autowired
+	private ContactRepository contactRepo;
+	
+	Contact contact1;
+	
+	@Before
+	public void setUp() {
+		contact1 = new Contact("Derek", "333-333-3333", "123 my address");
+		contactRepo.deleteAll();
+		contactRepo.save(contact1);
+		
+	}
+	
+	@Test
+	public void getContactByNameRepoTest() {
+		
+		final String NAMETOTEST = "Derek";
+		Contact contactToTestByName = contactRepo.findByName(NAMETOTEST);
+		assertNotNull(contactToTestByName);
+		assertEquals(contactToTestByName.getName(), NAMETOTEST);
+		
+	} 
+	
+	@Test
+	public void getContactByAddressRepoTest() {
+		final String ADDRESSTOTEST = "123 my address";
+		Contact contactToTestByAddress = contactRepo.findByAddress(ADDRESSTOTEST);
+		assertNotNull(contactToTestByAddress);
+		assertEquals(contactToTestByAddress.getAddress(), ADDRESSTOTEST);
+	}
+	
+	@Test
+	public void createContactTest() {
+		final String nameToExpect = "Libby";
+		Contact contactToTest = new Contact("Libby", "444-444-4444", "456 my address");
+		contactRepo.save(contactToTest);
+		Contact confirmContactToTest = contactRepo.findByName(nameToExpect);
+		assertNotNull(confirmContactToTest);
+		assertEquals(nameToExpect, confirmContactToTest.getName(), contactToTest.getName());
+	}
+	
+	
 	
 	
 	
